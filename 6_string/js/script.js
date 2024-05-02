86a140d974179012211d7eae96b764d8d2ddefeb
André Chaves <adg.chaves.rs@gmail.com>
2024-05-02 06:39:12 -0300

    Aula 6 - String

---
+++ 6_string/js/script.js
@@ -0,0 +1,23 @@
+var nome = "Andre";
+var sobrenome = "Chaves"
+
+var nomeCompleto = nome + " " + sobrenome;
+console.log(nome);
+console.log(typeof nome);
+
+console.log(typeof "asd");
+
+console.log(nomeCompleto);
+
+var frase = 'Esta é uma frase complexa';
+
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + " " + frase);
+
+document.write('Ele disse: "Olá"');
+
+document.write("ele disse: 'Olá'");
+
+console.log("Este número: " + nome);
