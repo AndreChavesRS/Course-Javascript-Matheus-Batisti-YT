7c1876ea418b1d35fa5a3824c13f7ef0d96db837
André Chaves <adg.chaves.rs@gmail.com>
2024-05-02 06:04:14 -0300

    Aula 4: Variaveis

--- 3_particulareidades_js/js/script.js
+++ 3_particulareidades_js/js/script.js
@@ -4,8 +4,19 @@ console.log("ultimo");
 
 console.log("Mais um console.log");
 
+
+//declara uma variavel
 var a = 1;
 
 a = "teste";
 
 console.log(a);
+
+// este é um comentario em uma linha
+
+/*
+este é um 
+comentario emm 
+multiplas linhas
+*/
+
---
+++ 4_variaveis/js/script.js
@@ -0,0 +1,44 @@
+var teste = 1;
+
+nome = "Pedro";
+
+console.log(teste);
+
+teste = "André";
+
+console.log(teste);
+
+console.log(nome);
+
+var $nome = 'ASD';
+var _nome = 'ASD2';
+
+console.log($nome);
+console.log(_nome);
+
+var nome5 = 'ASD3';
+
+console.log(nome5);
+
+var meuPrimeiroNome = 'André';
+
+console.log(meuPrimeiroNome);
+
+var meusobrenome = 'Chaves';
+
+console.log(meusobrenome);
+
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
+
+var meuNome;
+
+console.log(meuNome);
+
+meuNome = 'Andre Guilherme de Souza Chaves'
+
+console.log(meuNome);
+
