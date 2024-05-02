c51b223e836de1268885cb4218ee8c1dfc4cee88
André Chaves <adg.chaves.rs@gmail.com>
2024-05-02 06:23:58 -0300

    Aula 5: Number

---
+++ 5_number/js/script.js
@@ -0,0 +1,19 @@
+var numero = 5;
+
+console.log(numero);
+console.log(typeof numero);
+
+var float = 5.32;
+
+console.log(float);
+console.log(typeof float);
+
+var textoNumero = '523';
+
+console.log(textoNumero);
+console.log(typeof textoNumero);
+
+console.log(NaN);
+console.log(typeof NaN);
+console.log(typeof +Infinity);
+console.log(typeof -Infinity);
