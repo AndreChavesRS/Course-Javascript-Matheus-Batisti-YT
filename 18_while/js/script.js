fc2b3a30724d5e2a4e535ac2e76157a47517347c
André Chaves <adg.chaves.rs@gmail.com>
2024-05-02 09:43:50 -0300

    Aula 17 - While

---
+++ 18_while/js/script.js
@@ -0,0 +1,24 @@
+var x = 0;
+
+while (x < 5) {
+    console.log("Testando repetilçao " + x);
+
+    // incrementador
+    x++;
+}
+
+var arr = ["teste", "testando", "a", "b"];
+var y = 0;
+
+while (y <= 3) {
+    console.log(arr[y]);
+    y++;
+}
+
+var palavra = "Paralelepipedo";
+var i = 0;
+
+while (i <= 13) {
+    console.log(palavra[i]);
+    i++;
+}
