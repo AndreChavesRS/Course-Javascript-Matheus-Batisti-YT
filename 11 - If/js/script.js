b1a02a299f8ab7d8b75792071588d12ecd43ff90
André Chaves <adg.chaves.rs@gmail.com>
2024-05-02 07:19:06 -0300

    Aula 11 - IF

---
+++ 11 - If/js/script.js
@@ -0,0 +1,16 @@
+var idade = 19;
+
+console.log("Antes do if");
+
+if (true) {
+    console.log("Testando if");
+}
+
+console.log("Depois do if");
+
+if (idade >= 18) {
+    console.log("Pode fazer a carteira de motorista");
+
+}
+console.log("Nao pode fazer a carteira de motorista");
+
