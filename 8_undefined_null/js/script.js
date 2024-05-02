af2da067f1da023ab13013c79aeb36f81aaecd0a
André Chaves <adg.chaves.rs@gmail.com>
2024-05-02 06:53:18 -0300

    Aula 9 - Null e Undefined

---
+++ 8_undefined_null/js/script.js
@@ -0,0 +1,15 @@
+//Hoisting - içamento
+console.log(sobrenome);
+console.log(numero);
+
+var nome = null;
+var sobrenome;
+
+console.log(nome);
+console.log(sobrenome);
+
+nome = "André";
+
+console.log(nome);
+
+var numero = 5;
