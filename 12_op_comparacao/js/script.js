fa28d79dd66a363593e0ba32950e359b56d459f7
André Chaves <adg.chaves.rs@gmail.com>
2024-05-02 07:49:57 -0300

    Aula 13 - Else e Else IF

---
+++ 12_op_comparacao/js/script.js
@@ -0,0 +1,32 @@
+var idade = 17;
+var possuiCarro = 1;
+if (idade >= 18) {
+    console.log("O usuario pode fazer a carteira de motorista");
+}
+
+if (idade <= 17) {
+    console.log("O usuario nao pode fazer a carteira");
+
+}
+
+if (possuiCarro) {
+    console.log("O usuario pode pilotar um carro");
+}
+
+var nome = "Andre";
+
+if (nome == "Andre") {
+    console.log("O seu nome é Andre");
+}
+
+if (nome != "Andre") {
+    console.log("o seu nome nao é Andre");
+}
+
+if (20 > 10) {
+    console.log("Passou");
+}
+
+if (100 < 20) {
+    console.log("Passou2");
+}
---
+++ 13_else_elseif/js/script.js
@@ -0,0 +1,26 @@
+var nome = "Henrique";
+
+if (nome == "Quezia") {
+    console.log("O seu nome é Quezia");
+} else if (nome == "Andre") {
+    console.log("O nome é Andre");
+}
+else if (nome == "Henrique") {
+    console.log("O nome é Henrique");
+}
+else {
+    console.log("Ele possui outro nome");
+}
+var idade = 19;
+
+if (idade > 20) {
+    console.log("Ele pode entra na festa");
+} else if (idade >= 18) {
+    console.log("ele so pode entrar com solicitação");
+}
+
+if (nome == "Andre") {
+    console.log("teste");
+} else {
+    console.log("testando");
+}
